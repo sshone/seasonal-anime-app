619a89d3180040e63041c1c4f1ce7f1039c22789
Stuart Shone <stuartjshone@googlemail.com>
2023-05-03 02:16:11 +0100

    Stop using BuildContext across async context as can cause issues, such as the widget tree changing during the async call, replaced with ScaffoldMessenger

--- lib/screens/fullscreen_image.dart
+++ lib/screens/fullscreen_image.dart
@@ -7,53 +7,58 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class FullscreenImage extends StatelessWidget {
   final String imageUrl;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
 
-  const FullscreenImage({Key? key, required this.imageUrl}) : super(key: key);
+  FullscreenImage({Key? key, required this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          GestureDetector(
-            onTap: () => Navigator.pop(context),
-            onLongPress: () => _downloadImage(context, imageUrl),
-            child: Center(
-              child: InteractiveViewer(
-                panEnabled: false,
-                minScale: 1.0,
-                maxScale: 4.0,
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl,
-                  fit: BoxFit.contain,
-                  errorWidget: (context, url, error) => Image.asset(
-                    'assets/images/placeholderAnime.jpg',
+    return ScaffoldMessenger(
+      key: _scaffoldMessengerKey,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              onLongPress: () => _downloadImage(imageUrl),
+              child: Center(
+                child: InteractiveViewer(
+                  panEnabled: false,
+                  minScale: 1.0,
+                  maxScale: 4.0,
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl,
                     fit: BoxFit.contain,
+                    errorWidget: (context, url, error) => Image.asset(
+                      'assets/images/placeholderAnime.jpg',
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: 16,
-            right: 16,
-            child: IconButton(
-              onPressed: () => Navigator.pop(context),
-              icon: const Icon(Icons.close, color: Colors.white),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.close, color: Colors.white),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  Future<void> _downloadImage(BuildContext context, String imageUrl) async {
+  Future<void> _downloadImage(String imageUrl) async {
     try {
       final file = await DefaultCacheManager().getSingleFile(imageUrl);
       final bytes = await file.readAsBytes();
       final result = await ImageGallerySaver.saveImage(bytes);
-      ScaffoldMessenger.of(context).showSnackBar(
+      _scaffoldMessengerKey.currentState?.showSnackBar(
         SnackBar(
           content: Text(result != null
               ? 'Image successfully saved to gallery'
@@ -61,7 +66,7 @@ class FullscreenImage extends StatelessWidget {
         ),
       );
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
+      _scaffoldMessengerKey.currentState?.showSnackBar(
         SnackBar(
           content: Text('Error saving image to gallery: $e'),
         ),
